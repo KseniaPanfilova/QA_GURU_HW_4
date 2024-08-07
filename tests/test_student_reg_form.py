92ebee601c050493fff0f160452f3ba5808a44a6
Ksenia Panfilova <kseniapanfilova@gmail.com>
2024-08-07 23:36:31 +0300

    added tests steps

--- tests/test_student_reg_form.py
+++ tests/test_student_reg_form.py
@@ -11,5 +11,14 @@ def test_student_reg_form():
     browser.all('#genterWrapper label').should(have.texts('Male', 'Female', 'Other'))
     browser.all('#genterWrapper label').element_by(have.attribute('for', 'gender-radio-1')).click()
     browser.element('#userNumber').should(be.blank).type('1234567890')
-    browser.element('.react-datepicker-wrapper').click()
-    # browser.element('.react-datepicker__year-select').element()
+    browser.element('.react-datepicker-wrapper').click().element('#dateOfBirthInput').should(be.not_.blank)
+    browser.element('.react-datepicker__tab-loop').element('.react-datepicker__month-select').click().element(
+        '[value = "3"]').click()
+    browser.element('.react-datepicker__tab-loop').element('.react-datepicker__year-select').click().element(
+        '[value = "1985"]').click()
+    browser.all('.react-datepicker__month .react-datepicker__day').element_by(have.exact_text('13')).click()
+    browser.element('#subjectsInput').type('M')
+    browser.element('#react-select-2-option-0').click()
+    browser.all('#hobbiesWrapper .custom-control-label').should(have.texts('Sports', 'Reading', 'Music'))
+    browser.all('#hobbiesWrapper .custom-control-label').element_by(have.attribute('for', 'hobbies-checkbox-1')).click()
+    # browser.element('#uploadPicture')
